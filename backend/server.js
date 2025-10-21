31a5c84aa6b3e9e55471e5e813e84aef6ecf6dc3
Your Name <syd.ahad@gmail.com>
2025-10-21 10:19:38 +0530

    modified for deployment

--- backend/server.js
+++ backend/server.js
@@ -2,17 +2,21 @@
 import express from "express";
 import fetch from "node-fetch";
 import cors from "cors";
+import dotenv from 'dotenv'
+
+dotenv.config()
 
 const app = express();
 app.use(cors());
+const PORT = process.env.PORT || 4000
 
 app.get("/api/search", async (req, res) => {
   const query = req.query.q;
   const response = await fetch(
-    `https://suggestqueries.google.com/complete/search?client=firefox&ds=yt&q=${query}`
+    `${process.env.SEARCH_API}${query}`
   );
   const data = await response.json();
   res.json(data);
 });
 
-app.listen(4000, () => console.log("Server running on port 4000"));
+app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
